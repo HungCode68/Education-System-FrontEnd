a9460e47a2d768aae29553acbe98281a1373875d
HungCode68 <nguyenhungct6868@gmail.com>
2026-07-20 15:35:39 +0700

    change endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: ''
+  apiUrl: 'https://103.57.220.127'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@
 // };
 export const environment = {
   production: false,
-  apiUrl: ''
+  apiUrl: 'http://localhost:8081'
 };
 
