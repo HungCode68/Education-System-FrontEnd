c75b10c2eeb4aa39d0cd9ff236d5c1c0c83eee03
HungCode68 <nguyenhungct6868@gmail.com>
2026-08-28 00:20:27 +0700

    fix bug and update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // apiUrl:'https://103.57.220.127'
-  apiUrl:'http://localhost:8081'
+  apiUrl:'https://103.57.220.127'
+  // apiUrl:'http://localhost:8081'
 };
 
